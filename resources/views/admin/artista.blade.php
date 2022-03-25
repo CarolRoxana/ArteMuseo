8c9e317de429c5252e6ad2cdc5fa231e672f67dd
xbasir <xavier.basir@emqu.net>
2022-03-25 19:40:13 -0400

    fix: create artista blade

--- resources/views/admin/artista.blade.php
+++ resources/views/admin/artista.blade.php
@@ -9,63 +9,70 @@
 @section('content')
 <p class="text-center">Crear Artista</p>
 
-<div class="container">
-    <form class="bg-white py-2 px-4 shadow rounded mg-2" action="{{ '/admin/artista' }}" method="POST">
-        @csrf
-        <div class="row">
-            <div class="col-12 col-sm-10 col-lg-6 mx-auto">
-
-                <h4 class=" text-center ">Nombre artista</h4>
-                <hr>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="Name">
-
-                <br>
-                <h4 class=" text-center ">Apellido</h4>
-                <hr>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="LastaName">
-
-                <br>
-                <h4 class=" text-center">Email</h4>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="Email">
-                <hr>
-
-                <br>
-                <h4 class=" text-center">Fecha Nacimiento</h4>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="FechaNacimiento">
-                <hr>
-
-                <br>
-                <h4 class=" text-center">Nacionalidad</h4>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="Nacionalidad">
-                <hr>
-
-                <br>
-                <h4 class=" text-center">Biografia</h4>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="Biografia">
-                <hr>
-
-                <br>
-                <h4 class=" text-center">Porcentaje ganancias museo</h4>
-                <input class="form-control border-0 bg-light shadow-sm" type="text" name="Porcentaje">
-                <hr>
-
-                <div class="col-12 col-sm-10 col-lg-6 mx-auto" style="display: none">
-                    <h4 class=" text-center ">Bool</h4>
-                    <hr>
-                    <input class="form-control border-0 bg-light shadow-sm" type="text" value="/" name="img">
-                    <br>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <button type="submit" class="btn btn-primary">Registrar</button>
-                    </div>
-                </div>
-                <br>
-            </div>
+<form class="bg-white py-2 px-4 shadow rounded mg-2" action="{{ '/admin/artista' }}" method="POST">
+    @csrf
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Nombre artista</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" name="Name">
+            <br>
         </div>
-    </form>
-</div>
-
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Apellido</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" name="LastaName">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Email</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" name="Email">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Fecha Nacimiento</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="date" name="FechaNacimiento">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Nacionalidad</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" name="Nacionalidad">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Biografia</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" name="Biografia">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <h4 class=" text-center ">Porcentaje ganancias museo</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="number" name="Porcentaje">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto" style="display: none">
+            <h4 class=" text-center ">img</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="text" value="/" name="img">
+            <br>
+        </div>
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto" style="display: none">
+            <h4 class=" text-center ">Bool</h4>
+            <hr>
+            <input class="form-control border-0 bg-light shadow-sm" type="number" value="0" name="bool">
+            <br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Registrar</button>
+        </div>
+    </div>
+</form>
 
 <a href=""></a>
 <footer class="text-center bg-grey"> @ Todos lo derechos reservados </footer><br>
